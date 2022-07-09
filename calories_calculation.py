adb023d77eff18e98946fc611fdf7015df59c21b
Engineer Robert <kinyuaweru@gmail.com>
2022-07-09 23:31:50 +0300

    Added calories consumption calculation program

---
+++ calories_calculation.py
@@ -0,0 +1,18 @@
+def greet(client):
+    print("Hello, ", client, "\n")
+
+
+print("\nWelcome to Group 41 Nutritionists, Here we calculate your calories!\n")
+client_name = input("What is your name?")
+
+greet(client_name)
+
+carbs = int(input("How many grams of carbohydrates have you taken? "))
+fats = int(input("How many grams of fats have you taken? "))
+
+calories = (carbs * 4) + (fats * 9)
+
+print("\nFrom your Fats consumption: ", fats * 9, " Calories")
+print("From your Carbohydrates consumption: ", carbs * 4, " Calories")
+
+print("You have consumed ", calories, " calories,", client_name)
